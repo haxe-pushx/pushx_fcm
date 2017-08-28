2a89100f7ca068911018b08b01e8989e4072b161
Kevin Leung <kevinresol@gmail.com>
2017-08-28 14:36:08 +0800

    Constraints

--- src/pushx/fcm/FcmPusher.hx
+++ src/pushx/fcm/FcmPusher.hx
@@ -1,6 +1,6 @@
 package pushx.fcm;
 
-class FcmPusher<Data> extends pushx.gcm.GcmPusher<Data> {
+class FcmPusher<Data:{}> extends pushx.gcm.GcmPusher<Data> {
 	public function new(key) {
 		super(key);
 		url = 'https://fcm.googleapis.com/fcm/send';
